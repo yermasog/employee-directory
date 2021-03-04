8fd060e35b69086b364b34c612cdffc570b2fb01
ymasog <yer.masog@gmail.com>
2021-03-03 22:40:10 -0600

    npm installed axios and added axios call

--- src/components/RandomUserContainer.js
+++ src/components/RandomUserContainer.js
@@ -1,45 +1,52 @@
 import React, { Component } from "react";
 import Jumbotron from "./Jumbotron";
-// import API from "../utils/API";
+import Container from "./Container";
+import Row from "./Row";
+import API from "../utils/API";
 
 class RandomUserContainer extends Component {
-//   state = {
-//     result: {},
-//     search: ""
-//   };
+      state = {
+        result: {},
+        
+      };
 
-//   // When this component mounts, search for the movie "The Matrix"
-//   componentDidMount() {
-//     this.searchMovies("The Matrix");
-//   }
+      // When this component mounts, search for the movie "The Matrix"
+      componentDidMount() {
+        this.RenderEmployees();
+      }
 
-//   searchMovies = query => {
-//     API.search(query)
-//       .then(res => this.setState({ result: res.data }))
-//       .catch(err => console.log(err));
-//   };
+      RenderEmployees = () => {
+        API.search()
+          .then(res => this.setState({ result: res.data }))
+          .catch(err => console.log(err));
+      };
 
-//   handleInputChange = event => {
-//     const value = event.target.value;
-//     const name = event.target.name;
-//     this.setState({
-//       [name]: value
-//     });
-//   };
+    //   handleInputChange = event => {
+    //     const value = event.target.value;
+    //     const name = event.target.name;
+    //     this.setState({
+    //       [name]: value
+    //     });
+    //   };
 
-//   // When the form is submitted, search the OMDB API for the value of `this.state.search`
-//   handleFormSubmit = event => {
-//     event.preventDefault();
-//     this.searchMovies(this.state.search);
-//   };
+    //   // When the form is submitted, search the OMDB API for the value of `this.state.search`
+    //   handleFormSubmit = event => {
+    //     event.preventDefault();
+    //     this.searchMovies(this.state.search);
+    //   };
 
-  render() {
-    return (
-      <Jumbotron className="jumbotron">
-        <h1>Employee Directory</h1>  
-      </Jumbotron>
-    );
-  }
+    render() {
+        return (
+            <Container>
+                <Jumbotron className="jumbotron">
+                    <h1>Employee Directory</h1>
+                </Jumbotron>
+                <Row>
+                    
+                </Row>
+            </Container>
+        );
+    }
 }
 
 export default RandomUserContainer;
